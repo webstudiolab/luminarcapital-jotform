ab119f6e596fc269208d7728aabc32d55a5cfb5b
webstudiolab <82649462+webstudiolab@users.noreply.github.com>
2025-11-01 22:35:03 +0400

    Update index.ts

--- src/utils/email/index.ts
+++ src/utils/email/index.ts
@@ -10,10 +10,13 @@ export interface ISendEmail {
 const transporter = nodemailer.createTransport({
   host: process.env.SMTP_HOST,
   port: Number(process.env.SMTP_PORT),
-  secure: true, // true for port 465
+  secure: true,
   auth: {
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS,
+  },
+  tls: {
+    rejectUnauthorized: false,
   },
 } as TransportOptions)
 
